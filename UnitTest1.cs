6cc7a9532441ff9d16160025c980031a195515b3
wsderte <31801595+wsderte@users.noreply.github.com>
2020-10-12 21:23:02 +0300

    Test

---
+++ UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using Xunit;
+using IIG.BinaryFlag;
+
+
+namespace XUnitTestProject1{
+    public class UnitTest1 {
+
+        [Fact]
+        public void TestGetFlag() {
+            IIG.BinaryFlag.MultipleBinaryFlag Flag = new IIG.BinaryFlag.MultipleBinaryFlag(2, true);
+            bool a = Flag.GetFlag();
+            Assert.Equal(true, a);
+          
+        }
+
+        [Fact]
+        public void TestSetFlag()
+        {
+            IIG.BinaryFlag.MultipleBinaryFlag Flag = new IIG.BinaryFlag.MultipleBinaryFlag(2);
+            Flag.SetFlag(0);
+            bool a = Flag.GetFlag();
+            Assert.Equal(true, a);
+
+
+        }
+
+        [Fact]
+        public void TestREsetFlag()
+        {
+            IIG.BinaryFlag.MultipleBinaryFlag Flag = new IIG.BinaryFlag.MultipleBinaryFlag(2, true);
+            Flag.ResetFlag(1);
+            bool a = Flag.GetFlag();
+            Assert.Equal(false, a);
+
+
+        }
+
+       [Fact]
+        public void TestDispose()
+        {
+            IIG.BinaryFlag.MultipleBinaryFlag Flag = new IIG.BinaryFlag.MultipleBinaryFlag(2);
+            
+            Flag.SetFlag(1);
+            bool a = Flag.GetFlag();
+
+            Flag.Dispose();
+
+            Flag.SetFlag(1);
+            bool b = Flag.GetFlag();
+
+            Assert.NotEqual(a, b);
+            //  GC.SuppressFinalize();
+
+        }
+
+
+    }
+}
